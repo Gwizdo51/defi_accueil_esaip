4ef07f2bc5f8b55f4a09c0a1538011eb656520ee
Ravenophan <nerosheep@gmail.com>
2023-09-07 14:28:58 +0200

    added SpeakerPlacement in converter_base.py + added switch statement

--- converter_base.py
+++ converter_base.py
@@ -9,6 +9,7 @@ import math
 from measurement.measures import Speed, Distance, Weight
 from typing import Optional
 
+
 class Currency:
 
     def __init__(
@@ -35,96 +36,110 @@ class Currency:
                 self.init_val = self.dict_units[unit]
                 self.init_unit = unit
 
+ 
     @property
     def eur(self):
-        if self.init_unit == "eur":
-            return self.init_val
-        elif self.init_unit == "fr":
-            return self.init_val * 6.55957
-        elif self.init_unit == "usd":
-            return self.init_val * 0.93084
-        elif self.init_unit == "gbp":
-            return self.init_val * 1.1697
-        elif self.init_unit == "mxn":
-            return self.init_val / 18.84976302
-        elif self.init_unit == "jpy":
-            return self.init_val * 0.006325293888
+
+        match self.init_unit:
+            case "eur":
+                return self.init_val
+            case "fr":
+                return self.init_val * 6.55957
+            case "usd":
+                return self.init_val * 0.93084
+            case "gbp":
+                return self.init_val * 1.1697
+            case "mxn":
+                return self.init_val / 18.84976302
+            case "jpy":
+                return self.init_val * 0.006325293888
+
 
     @property
     def fr(self):
-        if self.init_unit == "eur":
-            return self.init_val / 6.55957
-        elif self.init_unit == "fr":
-            return self.init_val
-        elif self.init_unit == "usd":
-            return self.init_val * 6.1059379
-        elif self.init_unit == "gbp":
-            return self.init_val * 7.66549
-        elif self.init_unit == "mxn":
-            return self.init_val / 2.8382056613560
-        elif self.init_unit == "jpy":
-            return self.init_val * 0.041421886840
+
+         match self.init_unit:
+            case "eur":
+                return self.init_val  / 6.55957
+            case "fr":
+                return self.init.val
+            case "usd":
+                return self.init_val * 6.1059379
+            case "gbp":
+                return self.init_val * 7.66549
+            case "mxn":
+                return self.init_val / 2.8382056613560
+            case "jpy":
+                return self.init_val * 0.041421886840
 
     @property
     def usd(self):
-        if self.init_unit == "eur":
-            return self.init_val / 0.93084
-        elif self.init_unit == "fr":
-            return self.init_val / 6.1059379
-        elif self.init_unit == "usd":
-            return self.init_val
-        elif self.init_unit == "gbp":
-            return self.init_val * 1.25539
-        elif self.init_unit == "mxn":
-            return self.init_val / 17.5479
-        elif self.init_unit == "jpy":
-            return self.init_val * 0.006778788
+
+         match self.init_unit:
+            case "eur":
+                return self.init_val / 0.93084
+            case "fr":
+                return self.init_val / 6.1059379
+            case "usd":
+                return self.init_val * 0.93084
+            case "gbp":
+                return self.init_val * 1.25539
+            case "mxn":
+                return self.init_val / 17.5479
+            case "jpy":
+                return self.init_val * 0.006778788
 
     @property
     def gbp(self):
-        if self.init_unit == "eur":
-            return self.init_val / 1.1697
-        elif self.init_unit == "fr":
-            return self.init_val / 7.66549
-        elif self.init_unit == "usd":
-            return self.init_val / 1.25539
-        elif self.init_unit == "gbp":
-            return self.init_val
-        elif self.init_unit == "mxn":
-            return self.init_val / 21.99486
-        elif self.init_unit == "jpy":
-            return self.init_val * 0.005423217426
+
+         match self.init_unit:
+            case "eur":
+                return self.init_val / 1.1697
+            case "fr":
+                return self.init_val / 7.66549
+            case "usd":
+                return self.init_val / 1.25539
+            case "gbp":
+                return self.init_val
+            case "mxn":
+                return self.init_val / 21.99486
+            case "jpy":
+                return self.init_val * 0.005423217426
 
     @property
     def mxn(self):
-        if self.init_unit == "eur":
-            return self.init_val * 18.84976302
-        elif self.init_unit == "fr":
-            return self.init_val * 2.8382056613560
-        elif self.init_unit == "usd":
-            return self.init_val * 17.5479
-        elif self.init_unit == "gbp":
-            return self.init_val * 21.99486
-        elif self.init_unit == "mxn":
-            return self.init_val
-        elif self.init_unit == "jpy":
-            return self.init_val * 0.1195615647
+
+         match self.init_unit:
+            case "eur":
+                return self.init_val * 18.84976302
+            case "fr":
+                return self.init_val * 2.8382056613560
+            case "usd":
+                return self.init_val * 17.5479
+            case "gbp":
+                return self.init_val * 21.99486
+            case "mxn":
+                return self.init_val
+            case "jpy":
+                return self.init_val * 0.1195615647
 
     @property
     def jpy(self):
-        if self.init_unit == "eur":
-            return self.init_val / 0.006325293888
-        elif self.init_unit == "fr":
-            return self.init_val / 0.041421886840
-        elif self.init_unit == "usd":
-            return self.init_val / 0.006778788
-        elif self.init_unit == "gbp":
-            return self.init_val / 0.005423217426
-        elif self.init_unit == "mxn":
-            return self.init_val / 0.1195615647
-        elif self.init_unit == "jpy":
-            return self.init_val
 
+         match self.init_unit:
+            case "eur":
+                return self.init_val / 0.006325293888
+            case "fr":
+                return self.init_val / 0.041421886840
+            case "usd":
+                return self.init_val / 0.006778788
+            case "gbp":
+                return self.init_val / 0.005423217426
+            case "mxn":
+                return self.init_val / 0.1195615647
+            case "jpy":
+                return self.init_val
+            
 
 class Slope:
 
@@ -139,8 +154,6 @@ class Slope:
             if self.dict_units[unit] is not None:
                 self.init_val = self.dict_units[unit]
                 self.init_unit = unit
-       # print(self.init_val)
-       # print(self.init_unit)
 
     @property
     def rate(self):
@@ -181,5 +194,3 @@ def convert(value: float, unit_start: str, unit_end: str) -> float:
 
     measure_obj = dict_unit[unit_start]
     return measure_obj.__getattribute__(unit_end)
-
-print(convert(33.58, "km", "m"))
